36cf4289d4f97852b2d9c491f85de47ae51efc0a
KerwinJT <kerwinjt@gmail.com>
2020-03-27 13:57:11 -0600

    Updated Databased Connection, Tested User ent, Tested Springboot starts and reached webpage

--- JPAUrbanGarden/src/main/java/com/skilldistillery/urbangarden/entities/User.java
+++ JPAUrbanGarden/src/main/java/com/skilldistillery/urbangarden/entities/User.java
@@ -1,5 +1,70 @@
 package com.skilldistillery.urbangarden.entities;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	
+	public User() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + "]";
+	}
+
+	
 }
--- JPAUrbanGarden/src/test/java/com/skilldistillery/urbangarden/entities/UserTest.java
+++ JPAUrbanGarden/src/test/java/com/skilldistillery/urbangarden/entities/UserTest.java
@@ -1,34 +1,58 @@
 package com.skilldistillery.urbangarden.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	
+	static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+	
+	
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("UrbanGarden");
+		
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
+		
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("testing initial mapping of user entity")
+	void test1() {
+		assertNotNull(user);
+		assertEquals(1, user.getId());
+		assertEquals("jjsmith", user.getUsername());
+		
 	}
 
 }
