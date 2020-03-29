02c929e7a3c9cd09e57bec02ac18f6c49c5d70b8
Luke Sutherland <luke.sutherland@live.com>
2020-03-28 18:33:50 -0600

    created all entity controllers

---
+++ UrbanGarden/src/main/java/com/skilldistillery/urbangarden/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.urbangarden.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+		
+	@RequestMapping(path= {"/","home.do"})
+	public String home() {
+		return "index";
+	}
+}
