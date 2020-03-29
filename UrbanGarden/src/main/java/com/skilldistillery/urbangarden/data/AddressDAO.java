04002e7371fb33793b79518f1d0954013dd33fff
KerwinJT <kerwinjt@gmail.com>
2020-03-29 14:49:57 -0600

    DAOs for User, Address, StoreFront

--- UrbanGarden/src/main/java/com/skilldistillery/urbangarden/data/AddressDAO.java
+++ UrbanGarden/src/main/java/com/skilldistillery/urbangarden/data/AddressDAO.java
@@ -7,4 +7,8 @@ import com.skilldistillery.urbangarden.entities.Address;
 public interface AddressDAO {
 	Address findById(int id);
 	List<Address> findAll();
+	Address createAddress(Address address);
+	boolean destroyAddress(int id);
+	Address updateAddress(Address updateAdd);
+	
 }
