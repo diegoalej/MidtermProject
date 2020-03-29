ef3da8b6d314f5bd72bc9a94c7f49bfe793879b1
Luke Sutherland <luke.sutherland@live.com>
2020-03-28 19:05:46 -0600

    entity show/get controller methods added for review

--- UrbanGarden/src/main/java/com/skilldistillery/urbangarden/controllers/TradeController.java
+++ UrbanGarden/src/main/java/com/skilldistillery/urbangarden/controllers/TradeController.java
@@ -1,13 +1,26 @@
 package com.skilldistillery.urbangarden.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.skilldistillery.urbangarden.data.TradeDAO;
+import com.skilldistillery.urbangarden.entities.Trade;
 
 @Controller
 public class TradeController {
 		
-	@RequestMapping(path= {"/","home.do"})
-	public String home() {
-		return "index";
+	@Autowired
+	private TradeDAO dao;
+	
+	@RequestMapping(path = "getTrade.do", method = RequestMethod.GET, params = "id")
+	public String showTrade(@RequestParam Integer id, Model model) {
+		String view = "trade/show";
+		Trade trade = dao.findById(id);
+		model.addAttribute("trade", trade);
+		return view;
 	}
 }
--- UrbanGarden/src/main/java/com/skilldistillery/urbangarden/controllers/UserController.java
+++ UrbanGarden/src/main/java/com/skilldistillery/urbangarden/controllers/UserController.java
@@ -1,13 +1,26 @@
 package com.skilldistillery.urbangarden.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.skilldistillery.urbangarden.data.UserDAO;
+import com.skilldistillery.urbangarden.entities.User;
 
 @Controller
 public class UserController {
 		
-	@RequestMapping(path= {"/","home.do"})
-	public String home() {
-		return "index";
+	@Autowired
+	private UserDAO dao;
+	
+	@RequestMapping(path = "getUser.do", method = RequestMethod.GET, params = "id")
+	public String showUser(@RequestParam Integer id, Model model) {
+		String view = "user/show";
+		User user = dao.findById(id);
+		model.addAttribute("user", user);
+		return view;
 	}
 }
